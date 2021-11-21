3c327f68affe5165cca2517a538a01e27586ad01
Dmitry Shagurov <shagurovda@gmail.com>
2021-11-21 20:16:56 +0300

    html for login page

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import classNames from 'classnames';
 
 import '../styles/sass/pages/LoginPage.sass';
 
@@ -6,8 +7,50 @@ import '../styles/sass/pages/LoginPage.sass';
 function LoginPage() {
   return (
     <div className='login-page'>
-      <h1 className='login-page__label'>Sign in</h1>
-      <p className='login-page__invite-text'>Please login to your account</p>
+      
+      <div className='login-page__wrapper'>
+
+        <h1 className='login-page__label'>Sign in</h1>
+        <p className='login-page__invite-text'>Please login to your account</p>
+
+        <form className='login-form form'>
+          <input
+            className={classNames(
+              'login-form__email-field',
+              'form__field',
+              'form__email-field'
+            )}
+            type='email'
+            name='login'
+            placeholder='Your email'
+            required
+          />
+          <input
+            className={classNames(
+              'login-form__password-field',
+              'form__field',
+              'form__password-field'
+            )}
+            type='password'
+            name='password'
+            placeholder='Password'
+            required
+          />
+          <input
+            className={classNames(
+              'login-form__submit',
+              'form__submit',
+            )}
+            type='submit'
+          />
+        </form>
+
+        <a className='login-form__redirect-link' href="#">
+          Register now
+        </a>
+      
+      </div>
+
     </div>
   )
 }
