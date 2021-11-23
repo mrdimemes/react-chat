85ce109fceb773f06b94b3d6aee22db44e512565
Dmitry Shagurov <shagurovda@gmail.com>
2021-11-23 14:51:14 +0300

    login page styles

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -13,42 +13,44 @@ function LoginPage() {
         <h1 className='login-page__label'>Sign in</h1>
         <p className='login-page__invite-text'>Please login to your account</p>
 
-        <form className='login-form form'>
-          <input
-            className={classNames(
-              'login-form__email-field',
-              'form__field',
-              'form__email-field'
-            )}
-            type='email'
-            name='login'
-            placeholder='Your email'
-            required
-          />
-          <input
-            className={classNames(
-              'login-form__password-field',
-              'form__field',
-              'form__password-field'
-            )}
-            type='password'
-            name='password'
-            placeholder='Password'
-            required
-          />
-          <input
-            className={classNames(
-              'login-form__submit',
-              'form__submit',
-            )}
-            type='submit'
-          />
-        </form>
+        <div className='login-page__form'>
+          <form className='login-form form'>
+            <input
+              className={classNames(
+                'login-form__email-field',
+                'form__field',
+                'form__email-field'
+              )}
+              type='email'
+              name='login'
+              placeholder='Your email'
+              required
+            />
+            <input
+              className={classNames(
+                'login-form__password-field',
+                'form__field',
+                'form__password-field'
+              )}
+              type='password'
+              name='password'
+              placeholder='Password'
+              required
+            />
+            <input
+              className={classNames(
+                'login-form__submit',
+                'form__submit',
+              )}
+              type='submit'
+            />
+          </form>
 
-        <a className='login-form__redirect-link' href="/">
-          Register now
-        </a>
-      
+          <a className='login-page__redirect-link' href="/">
+            Register now
+          </a>
+        </div>
+
       </div>
 
     </div>
