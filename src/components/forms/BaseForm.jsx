114bf5de16f4039c0a0f0ad55c761a3408da5640
Dmitry Shagurov <shagurovda@gmail.com>
2022-01-16 20:08:54 +0400

    Comments and prop types for the BaseForm component

--- src/components/forms/BaseForm.jsx
+++ src/components/forms/BaseForm.jsx
@@ -1,5 +1,22 @@
 import React from 'react';
+
+import PropTypes from 'prop-types';
+
 import classNames from 'classnames';
+
+
+// The base form component. Renders an html form with an arbitrary number of
+// required fields and a submit button.
+
+// Component props:
+
+// className: string - extended component class name;
+// fields: array of arrays of the form
+//     [fieldType, fieldName, fieldPlaceholder], where:
+//         fieldType: string - input type and part of additional class;
+//         fieldName: string - input name;
+//         fieldPlaceholder: string - input placeholder;
+// submitButtonText: string.
 
 
 function BaseForm({ className, fields, submitButtonText }) {
@@ -35,5 +52,17 @@ function BaseForm({ className, fields, submitButtonText }) {
     </form>
   )
 }
+
+
+BaseForm.defaultProps = {
+  submitButtonText: 'submit'
+}
+
+BaseForm.propTypes = {
+  className: PropTypes.string,
+  fields: PropTypes.arrayOf(PropTypes.string).isRequired,
+  submitButtonText: PropTypes.string,
+}
+
 
 export default BaseForm;
