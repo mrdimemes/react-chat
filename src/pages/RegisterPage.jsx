52e08289e9a7cb353b31a6f03b0e4fb72190343f
Dmitry Shagurov <shagurovda@gmail.com>
2022-01-16 17:31:47 +0400

    register page refactoring

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,27 +1,23 @@
 import React from 'react';
 
+import { BaseFormPage } from './';
 import { RegisterForm } from '../components'
+
 import '../styles/sass/pages/RegisterPage.sass';
 
 
 function RegisterPage() {
   return (
-    <div className='register-page'>
-      <div className='register-page__wrapper'>
-
-        <h1 className='register-page__label'>Registration</h1>
-        <p className='register-page__invite-text'>To enter the chat, you need to register</p>
-
-        <div className='register-page__form'>
-          < RegisterForm />
-          <a className='register-page__redirect-link' href="/">
-            Sign in to your account
-          </a>
-        </div>
-
-      </div>
-    </div>
+    <BaseFormPage
+      className='register-page'
+      label='Registration'
+      inviteText='To enter the chat, you need to register'
+      redirectText='Sign in to your account'
+      redirectUrl='/'
+      form={ <RegisterForm /> }
+    />
   )
 }
+
 
 export default RegisterPage;
