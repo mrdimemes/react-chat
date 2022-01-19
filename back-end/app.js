415ba5227a244af687602e7d474adf73eb84dee8
Dmitry Shagurov <shagurovda@gmail.com>
2022-01-19 18:19:25 +0400

    MySQL integration

--- back-end/app.js
+++ back-end/app.js
@@ -2,6 +2,14 @@ const express = require('express');
 const app = express();
 const port = 3001;
 
+const mysql = require('mysql');
+const databaseConnection = mysql.createConnection({
+  host: 'localhost',
+  user: 'user',
+  password: 'password',
+  database: 'react_chat_database'
+});
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 });
