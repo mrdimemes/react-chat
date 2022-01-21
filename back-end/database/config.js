6bf6e974a4d78a274d8b99c2eea64b54b398382d
Dmitry Shagurov <shagurovda@gmail.com>
2022-01-21 16:46:32 +0400

    Added db creation script

--- back-end/database/config.js
+++
@@ -1,6 +0,0 @@
-// Enter your config here
-
-export const host = 'localhost';
-export const userName = 'root';
-export const password = 'password';
-export const databaseName = 'react_chat';
---
+++ back-end/database/create.js
@@ -0,0 +1,35 @@
+import mysql from 'mysql';
+import 'dotenv/config';
+
+
+// This file is not the part of application!
+// Run it via node to create MySQL database and tables
+
+
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+});
+
+connection.connect((err) => {if (err) {throw (err)}});
+
+connection.query(
+  `CREATE DATABASE ${process.env.BD_NAME}`,
+  (err) => {if (err) {throw (err)}}
+);
+connection.query(
+  `USE ${process.env.BD_NAME}`,
+  (err) => {if (err) {throw (err)}}
+);
+connection.query(
+  'CREATE TABLE users ( ' + 
+  'id INT PRIMARY KEY AUTO_INCREMENT NOT NULL, ' +
+  'email VARCHAR(50) NOT NULL, ' +
+  'password VARCHAR(50) NOT NULL, ' +
+  'name VARCHAR(50) NOT NULL ' +
+  ')',
+  (err) => {if (err) {throw (err)}}
+);
+
+connection.end((err) => {if (err) {throw (err)}});
