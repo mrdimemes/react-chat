b14973d5087563d8e7b14cc0712a5b29346651da
Dmitry Shagurov <shagurovda@gmail.com>
2022-01-21 18:57:49 +0400

    chat html

--- front-end/src/pages/ChatPage.jsx
+++ front-end/src/pages/ChatPage.jsx
@@ -14,8 +14,64 @@ function ChatPage() {
           <div className='chat__header-popup'></div>
         </div>
 
-        <div className='chat__massage-gallery massage-gallery'></div>
-        <div className='chat__input-bar'></div>
+        <div className='chat__massage-gallery massage-gallery'>
+
+          <div className='massage-gallery massage massage_incoming'>
+            <div className='massage__photo'></div>
+            <div className='massage__text massage__text_incoming'>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Proin maximus tempus risus non ultricies. Aliquam erat volutpat.
+              Nulla elementum velit sed efficitur vehicula. Morbi velit lorem,
+              imperdiet pellentesque est sed, dictum finibus nisl.
+            </div>
+            <div className='massage__marker'></div>
+            <div className='massage__time'></div>
+          </div>
+
+          <div className='massage-gallery massage massage_incoming'>
+            <div className='massage__photo'></div>
+            <div className='massage__text massage__text_incoming'>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            </div>
+            <div className='massage__marker'></div>
+            <div className='massage__time'></div>
+          </div>
+
+          <div className='massage-gallery massage massage_incoming'>
+            <div className='massage__photo'></div>
+            <div className='massage__text massage__text_incoming'>
+              Lorem ipsum
+            </div>
+            <div className='massage__marker'></div>
+            <div className='massage__time'></div>
+          </div>
+
+          <div className='massage-gallery massage massage_outgoing'>
+            <div className='massage__photo'></div>
+            <div className='massage__text massage__text_outgoing'>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Proin maximus tempus risus non ultricies. Aliquam erat volutpat.
+              Nulla elementum velit sed efficitur vehicula. Morbi velit lorem,
+              imperdiet pellentesque est sed, dictum finibus nisl.
+            </div>
+            <div className='massage__marker'></div>
+            <div className='massage__time'></div>
+          </div>
+
+          <div className='massage-gallery massage massage_outgoing'>
+            <div className='massage__photo'></div>
+            <div className='massage__text massage__text_outgoing'>
+              Lorem ipsum
+            </div>
+            <div className='massage__marker'></div>
+            <div className='massage__time'></div>
+          </div>
+
+        </div>
+        <form className='chat__input'>
+          <input className='chat__input-text' type='text' />
+          <input className='chat__input-submit' type='submit' />
+        </form>
 
       </div>
 
