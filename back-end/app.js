464892f6bcd6048159175fc22b35a897158b5175
Dmitry Shagurov <shagurovda@gmail.com>
2022-01-21 16:02:13 +0400

    pool for db connections

--- back-end/app.js
+++ back-end/app.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import 'dotenv/config';
-import databaseConnection from './database/index.js';
+import databasePool from './database/index.js';
 
 
 const app = express();
@@ -12,6 +12,6 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 });
 
-app.listen(port, () => {
+app.listen(PORT, () => {
   console.log(`App listening at http://localhost:${PORT}`)
 });
--- back-end/database/index.js
+++ back-end/database/index.js
@@ -2,11 +2,13 @@ import mysql from 'mysql';
 import 'dotenv/config';
 
 
-const databaseConnection = mysql.createConnection({
+const pool = mysql.createPoll({
+  connectionLimit: process.env.DB_CONNECTION_LIMIT,
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME
 });
+
 
 export default databaseConnection;
