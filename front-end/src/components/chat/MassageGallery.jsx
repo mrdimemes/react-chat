9dc98c2781ae14d9be04de3fa8f1e1b76094fefa
Dmitry Shagurov <shagurovda@gmail.com>
2022-01-23 20:07:40 +0400

    massage gallery scrolling

--- front-end/src/components/chat/MassageGallery.jsx
+++ front-end/src/components/chat/MassageGallery.jsx
@@ -18,10 +18,40 @@ function MassageGallery() {
       />
 
       <Massage
+        type='outgoing'
+        children='Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          Proin maximus tempus risus non ultricies. Aliquam erat volutpat.
+          Nulla elementum velit sed efficitur vehicula. Morbi velit lorem,
+          imperdiet pellentesque est sed, dictum finibus nisl.'
+        time='timestamp'
+        autor='Brute'
+      />
+
+      <Massage
         type='incoming'
         children='Lorem ipsum dolor sit amet, consectetur'
         time='timestamp'
         autor='Caesar'
+      />
+
+      <Massage
+        type='incoming'
+        children='Lorem ipsum'
+      />
+
+      <Massage
+        type='outgoing'
+        children='Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          Proin maximus tempus risus non ultricies. Aliquam erat volutpat.
+          Nulla elementum velit sed efficitur vehicula. Morbi velit lorem,
+          imperdiet pellentesque est sed, dictum finibus nisl.'
+        time='timestamp'
+        autor='Brute'
+      />
+
+      <Massage
+        type='outgoing'
+        children='Lorem ipsum'
       />
 
       <Massage
