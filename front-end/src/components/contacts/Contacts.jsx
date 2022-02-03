03e8eca7c769efade2a2aaea0ec83d6b3bae6f57
Dmitry Shagurov <shagurovda@gmail.com>
2022-02-03 20:33:00 +0300

    styles for contacts component, icons png

--- front-end/src/components/contacts/Contacts.jsx
+++ front-end/src/components/contacts/Contacts.jsx
@@ -1,11 +1,34 @@
 import React from 'react';
 
+import '../../styles/sass/components/contacts/Contacts.sass';
+
+import contactsPng from '../../assets/images/png/contacts.png';
+import newChatPng from '../../assets/images/png/new-chat.png';
+
+
 
 function Contacts() {
-    return (
-        <div className='contacts'>
+  return (
+    <div className='contacts'>
+      <div className='contacts__header'>
+        <div className='contacts__popup'>
+          <div className='contacts__popup-icon'>
+            <img src={ contactsPng } alt='contacts' />
+          </div>
+          <div className='contacts__popup-text'>Contacts</div>
         </div>
-    )
+        <div className='contacts__new-chat'>
+          <img src={ newChatPng } alt='new chat' />
+        </div>
+      </div>
+      <div className='contacts__search'>
+        search
+      </div>
+      <div className='contacts__gallery'>
+        gallery
+      </div>
+    </div>
+  )
 }
 
 
