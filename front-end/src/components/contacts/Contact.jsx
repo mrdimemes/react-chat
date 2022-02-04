cd5bda6c01397bf3b7c84efa957430d1ccda91bd
Dmitry Shagurov <shagurovda@gmail.com>
2022-02-04 18:51:19 +0300

    styles for contact component

--- front-end/src/components/contacts/Contact.jsx
+++ front-end/src/components/contacts/Contact.jsx
@@ -1,8 +1,22 @@
 import React from 'react';
 
+import '../../styles/sass/components/contacts/Contact.sass';
+
+
 function Contact() {
   return (
-    <div className='contact'>item</div>
+    <div className='contact'>
+      <div className='contact__photo'>
+        <div className='contact__online-marker'></div>
+      </div>
+      <div className='contact__body'>
+        <h3 className='contact__name'>Jack the Ripper</h3>
+        <p className='contact__preview'>Blablabla bla-bla blaaaaaaaaaaaa</p>
+        <div className='contact__unreaded-income'>3</div>
+        <div className='contact__outcome-status'></div>
+        <div className='contact__time'>14:32</div>
+      </div>
+    </div>
   )
 }
 
