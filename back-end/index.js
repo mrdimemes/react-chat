0396e6cef88f37b97fea12f069e5a02ed12a418b
Dmitry Shagurov <shagurovda@gmail.com>
2022-02-22 16:01:37 +0300

    end points added

--- back-end/index.js
+++ back-end/index.js
@@ -2,13 +2,15 @@ import 'dotenv/config';
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import router from "./router/index.js"
 
 const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors())
+app.use(cors());
+app.use("/", router);
 
 const start = async () => {
   try {
