47a59ba826e31c1bb54406b59a80066ab26bb0f2
Dmitry Shagurov <shagurovda@gmail.com>
2022-02-23 14:03:29 +0300

    dependencies included

--- back-end/index.js
+++ back-end/index.js
@@ -2,7 +2,7 @@ import 'dotenv/config';
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-import router from "./router/index.js"
+import router from "./apps/auth/router/index.js"
 
 const PORT = process.env.PORT || 5000;
 const app = express();
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors());
-app.use("/", router);
+app.use("/auth", router);
 
 const start = async () => {
   try {
