fbe11627efae8067988c27d54ff2a08f4bcca0e0
Dmitry Shagurov <shagurovda@gmail.com>
2022-02-24 18:35:23 +0300

    mysql-connector user-controller fixes, new functional for removing users from db

--- back-end/apps/mysql-connector/controllers/user-controller.js
+++ back-end/apps/mysql-connector/controllers/user-controller.js
@@ -4,21 +4,24 @@ class UserController {
   _connector = connector;
 
   async addUser(name, email, password) {
-    const sql = "INSERT INTO users(name, email, password) VALUES (?, ?, ?)";
+    const sql = "INSERT INTO users (name, email, password) VALUES (?, ?, ?)";
     await this._connector.query(sql, [name, email, password]);
   }
 
   async findUserByEmail(email) {
-    const sql = "SELECT * FROM users WHERE email=?";
+    const sql = "SELECT * FROM users WHERE email = ?";
     return await this._connector.query(sql, [email]);
   }
 
   async findUserById(id) {
-    const sql = "SELECT * FROM users WHERE email=?";
+    const sql = "SELECT * FROM users WHERE id = ?";
     return await this._connector.query(sql, [id]);
   }
 
-  async removeUser(id) {}
+  async removeUser(id) {
+    const sql = "DELETE FROM users WHERE id = ?";
+    await this._connector.query(sql, [id]);
+  }
 }
 
 
