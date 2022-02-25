88e54589320a543fde45c7603ec36aa44040d219
Dmitry Shagurov <shagurovda@gmail.com>
2022-02-25 16:13:14 +0300

    added token service with web tokens generation functional

--- back-end/apps/auth/contrillers/services/token-sercive.js
+++ back-end/apps/auth/contrillers/services/token-sercive.js
@@ -1,5 +1,30 @@
-class TokenService {
+import "dotenv/config";
+import Jwt from "jsonwebtoken";
 
+
+class TokenService {
+  _jwt;
+
+  constructor() {
+    this._jwt = Jwt;
+  }
+
+  generateTokens(payload) {
+    const accessToken = this._jwt.sign(
+      payload,
+      process.env.JWT_ACCESS_SERCRET,
+      { expiresIn: "30m" }
+    );
+    const refreshToken = this._jwt.sign(
+      payload,
+      process.env.JWT_REFRESH_SERCRET,
+      { expiresIn: "30d" }
+    );
+    return {
+      accessToken,
+      refreshToken
+    }
+  }
 }
 
 export default new TokenService()
