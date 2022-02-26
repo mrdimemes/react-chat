a7b83cb7e003d57334617d7ae6d5d7623f8dbf2a
Dmitry Shagurov <shagurovda@gmail.com>
2022-02-26 20:47:41 +0300

    sending activation mails functional added

--- back-end/apps/auth/contrillers/services/mail-service.js
+++ back-end/apps/auth/contrillers/services/mail-service.js
@@ -1,6 +1,41 @@
-class MailService {
-  async sendActivationMail(to, link) {
+import "dotenv/config";
+import nodemailer from "nodemailer";
 
+
+class MailService {
+
+  _transporter;
+
+  constructor() {
+    this._transporter = nodemailer.createTransport({
+      host: process.env.SMTP_HOST,
+      port: process.env.SMTP_PORT,
+      secure: true,
+      auth: {
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASSWORD
+      }
+    })
+  }
+
+  async sendActivationMail(to, link) {
+    const activationURL = process.env.HOST_URL +
+      process.env.AUTH_PATH +
+      "/activate/"
+      + link;
+    await this._transporter.sendMail({
+      from: process.env.SMTP_USER,
+      to: to,
+      subject: "Accaunt activation: " + process.env.HOST_URL,
+      text: "",
+      html: 
+        `
+          <div>
+            <h1>To activate your account follow the link</h1>
+            <a href="${activationURL}">${activationURL}</a>
+          </div>
+        `
+    });
   }
 }
 
