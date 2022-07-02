c70dbeb84cc38ee128712fd4312b8d557b1974d7
Dmitry Shagurov <shagurovda@gmail.com>
2022-07-02 16:33:32 +0400

    front-end: new login form

--- front-end/src/components/forms/LoginForm.tsx
+++ front-end/src/components/forms/LoginForm.tsx
@@ -1,18 +1,37 @@
-import React from 'react';
-import { BaseForm } from './';
+import React, { FC, useState } from "react";
 
 
-function LoginForm() {
+const LoginForm: FC = () => {
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
   return (
-    <BaseForm 
-      className='login-form'
-      submitButtonText='Sign in'
-      fields={[
-        ['email', 'login', 'E-mail'],
-        ['password', 'password', 'Password']
-      ]}
-    />
+    <form className="form login-form">
+
+      <input
+        className="form__field login-form__field"
+        type="email"
+        name="email"
+        placeholder="E-mail"
+        onChange={e => setEmail(e.target.value)}
+      />
+
+      <input
+        className="form__field login-form__field"
+        type="password"
+        name="password"
+        placeholder="Password"
+        onChange={e => setPassword(e.target.value)}
+      />
+
+      <button
+        className="form__submit login-form__submit"
+      >
+        Log in
+      </button>
+    </form>
   )
 }
+
 
 export default LoginForm;
