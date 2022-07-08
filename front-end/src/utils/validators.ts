2fa677e1524dc81a1773e24e777408fcfe45e4ca
Dmitry Shagurov <shagurovda@gmail.com>
2022-07-08 19:55:53 +0400

    front-end: undefined case fix for form fields validators

--- front-end/src/utils/validators.ts
+++ front-end/src/utils/validators.ts
@@ -1,9 +1,19 @@
-export const validateEmail = (email: string): boolean => {
+export const validateEmail = (email: string | undefined): boolean => {
+  if (typeof email === "undefined") {
+    return false
+  }
   const re: RegExp = new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
   return re.test(email);
 }
 
-export const validateString = (string: string, minLength=6, maxLength=25): [boolean, string] => {
+export const validateString = (
+  string: string | undefined,
+  minLength = 6,
+  maxLength = 30
+): [boolean, string] => {
+  if (typeof string === "undefined") {
+    return [false, "incorrect value"]
+  }
   if (string.length < minLength) {
     return [false, "too short"];
   }
