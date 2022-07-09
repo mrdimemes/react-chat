554b24c3c22563e5f1d4bba01b60bf611f6fdc34
Dmitry Shagurov <shagurovda@gmail.com>
2022-07-09 18:10:02 +0400

    front-end: registration form validator

--- front-end/src/utils/validators.ts
+++ front-end/src/utils/validators.ts
@@ -1,9 +1,15 @@
-export const validateEmail = (email: string | undefined): boolean => {
+import { RegistrationErrors } from "../components/forms/types";
+
+export const validateEmail = (email: string | undefined): [boolean, string] => {
   if (typeof email === "undefined") {
-    return false
+    return [false, "incorrect value"]
+  }
+  if (email.length === 0) {
+    return [false, "empty value"];
   }
   const re: RegExp = new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
-  return re.test(email);
+  const isValid: boolean = re.test(email);
+  return [isValid, isValid?"":"invalid e-mail"];
 }
 
 export const validateString = (
@@ -13,6 +19,9 @@ export const validateString = (
 ): [boolean, string] => {
   if (typeof string === "undefined") {
     return [false, "incorrect value"]
+  }
+  if (string.length === 0) {
+    return [false, "empty value"];
   }
   if (string.length < minLength) {
     return [false, "too short"];
@@ -25,4 +34,23 @@ export const validateString = (
     return [false, "incorrect charset"];
   }
   return [true, ""];
+}
+
+export const validateRegistrationForm = (
+  email: string | undefined,
+  name: string | undefined,
+  password: string | undefined,
+  passwordConfirmation: string | undefined,
+): RegistrationErrors => {
+  const formErrors: RegistrationErrors = {} as RegistrationErrors;
+  const [isEmailCorrect, emailErr] = validateEmail(name);
+  if (!isEmailCorrect) { formErrors.emailError = emailErr }
+  const [isNameCorrect, nameErr] = validateString(name);
+  if (!isNameCorrect) { formErrors.nameError = nameErr }
+  const [isPasswordCorrect, passErr] = validateString(password);
+  if (!isPasswordCorrect) { formErrors.passwordError = passErr }
+  if (password !== passwordConfirmation) {
+    formErrors.passwordError = "passwords do not match"
+  }
+  return formErrors;
 }
